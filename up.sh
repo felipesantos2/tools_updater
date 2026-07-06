9b684562d8072193c3ccea4cb536b15c7930d8c0
felipesantos2 <santospinheiro627@gmail.com>
2026-07-06 10:50:41 -0300

    wip

--- up.sh
+++ up.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 ## update cli tools
+
 uv self update
 rm -f /home/felipesantos2/.local/bin/zoxide
 curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
